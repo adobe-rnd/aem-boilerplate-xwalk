79af9b2daa9e36ac6f139d57a65e700bafb79508
pashtepooja18 <poojapashte18@gmail.com>
2024-08-01 15:44:20 +0530

    delete unwanted file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -702,7 +702,7 @@ async function loadingCustomCss() {
     `${window.hlx.codeBasePath}/styles/legal/legal.css`,
     `${window.hlx.codeBasePath}/styles/calculator-mob-carousel/calculator-mob-carousel.css`,
     `${window.hlx.codeBasePath}/styles/media/media-list.css`,
-    `${window.hlx.codeBasePath}/styles/authorisedagencies/authorisedagenciesbanner.css`,
+    `${window.hlx.codeBasePath}/styles/authorisedagencies/authorisedagencies.css`,
   ];
 
   loadCssArray.forEach(async (eachCss) => {
