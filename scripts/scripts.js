f75100d93ce60cd0161b675d29f5658e92b0826e
Smit Shah <smit.shah@teknopoint.in>
2024-08-01 13:07:21 +0530

    z-index 0 removed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -719,7 +719,7 @@ body?.addEventListener("click", function (e) {
     document.body.style.overflow = "scroll";
     document.querySelector(".modal-overlay").classList.remove("overlay");
     document.querySelector(".modal-overlay").classList.add("dp-none");
-    document.querySelector(".modal-overlay").style.zIndex = 0;
+    // document.querySelector(".modal-overlay").style.zIndex = 0;
   } else if (!e.target.closest(".nav-drop")) {
     //console.log("don't close nav");
 
