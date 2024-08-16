ad542e8c94d40d4d3dc84804b0b6f82d4d10e933
Smit Shah <smit.shah@teknopoint.in>
2024-08-17 01:12:57 +0530

    glider

--- blocks/tab-name/tab-name.js
+++ blocks/tab-name/tab-name.js
@@ -71,7 +71,7 @@ export function generateTabName(block) {
             Array.from(nestedTabName.children[0].children).forEach(function (eachTab) {
                 eachTab.classList.remove("active");
             })
-            if(classes == "glider"){
+            if(classes == "glider" || nestedTabName.classList.contains('glider-int')){
                 firsttab = nestedTabName.children[0].children[0].children[0];
             }else{
                 firsttab = nestedTabName.children[0].children[0];
