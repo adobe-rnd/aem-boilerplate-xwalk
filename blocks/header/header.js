a5f9163677e6ec7cadfc49a1b443600856f7ec0e
Smit Shah <smit.shah@teknopoint.in>
2024-08-23 20:30:38 +0530

    header handling

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -81,7 +81,7 @@ function focusNavSection() {
  */
 export function toggleAllNavSections(sections, expanded = false) {
     sections
-        .querySelectorAll('.nav-sections .default-content-wrapper > ul > li')
+        ?.querySelectorAll('.nav-sections .default-content-wrapper > ul > li')
         .forEach((section) => {
             // section.classList.toggle('active');
             section.setAttribute('aria-expanded', expanded);
@@ -102,9 +102,9 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
     toggleAllNavSections(navSections, expanded || isDesktop.matches ? 'false' : 'true');
     button.setAttribute('aria-label', expanded ? 'Open navigation' : 'Close navigation');
     // enable nav dropdown keyboard accessibility
-    const navDrops = navSections.querySelectorAll('.nav-drop');
+    const navDrops = navSections?.querySelectorAll('.nav-drop');
     if (isDesktop.matches) {
-        navDrops.forEach((drop) => {
+        navDrops?.forEach((drop) => {
             if (!drop.hasAttribute('tabindex')) {
                 drop.setAttribute('role', 'button');
                 drop.setAttribute('tabindex', 0);
@@ -112,7 +112,7 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
             }
         });
     } else {
-        navDrops.forEach((drop) => {
+        navDrops?.forEach((drop) => {
             drop.removeAttribute('role');
             drop.removeAttribute('tabindex');
             drop.removeEventListener('focus', focusNavSection);
