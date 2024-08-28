16d1dfc27f1ec922082e229a1a76cf6bfb69808a
Smit Shah <smit.shah@teknopoint.in>
2024-08-28 20:28:38 +0530

    dropdown logic integrated

--- blocks/branchlocator/branchlocator.js
+++ blocks/branchlocator/branchlocator.js
@@ -1,5 +1,6 @@
 import { loadScript } from "../../scripts/aem.js";
 import returnLatLan, { locationInLatLan } from "../select-tag/getSelectedLanguage.js";
+import { onloadBranchLocator } from "./branchlocator-biz.js";
 
 
 export function branchLocator_dropdown(_block){
@@ -7,24 +8,20 @@ export function branchLocator_dropdown(_block){
    let html =  `<div class='container dropdown-container'> 
                                 <div class='dropdown-wrapper'>
 
-                                    <div class='state-dropdown dropdown'>
-                                        <div class='dropdown-selectvalue'> Maharashtra </div>
-                                        <ul class='state-vlaue-option dropdown-option-wrapper'>
-                                            <input type='text' placeholder='State' id='search'/>
+                                    <div class='state-dropdown dropdown dropdown-li-logic'>
+                                        <div class='dropdown-selectvalue deafult-state-selected'> Maharashtra </div>
+                                        <ul class='state-vlaue-option dropdown-option-wrapper state-wrapper dp-none'>
+                                            <input type='text' placeholder='State' id='search' class="search-input"/>
                                              <div class='option-wrapper'>
-                                                <li class='state-option option'> Maharashtra </li>
                                              </div>
                                         </ul>
                                     </div>
 
-                                    <div class='city-dropdown dropdown'>
-                                        <div class='dropdown-selectvalue'> Mumbai </div>
-                                        <ul class='city-vlaue-option dropdown-option-wrapper dp-none'>
-                                            <input type='text' placeholder='City' id='search'/>
+                                    <div class='city-dropdown dropdown dropdown-li-logic'>
+                                        <div class='dropdown-selectvalue deafult-city-selected'> Mumbai </div>
+                                        <ul class='city-vlaue-option dropdown-option-wrapper city-wrapper dp-none'>
+                                            <input type='text' placeholder='City' id='search' class="search-input"/>
                                             <div class='option-wrapper'>
-                                                <li class='city-option option'> Mumbai </li>
-                                                <li class='city-option option'> Mumbai </li>
-                                                <li class='city-option option'> Mumbai </li>
                                             </div>
                                         </ul>
                                     </div>
@@ -133,53 +130,14 @@ export function branchLocator() {
     return branch_cards;
 }
 export default function decorate(block) {
-    // branchLocator_dropdown();
-    // branchLocator_Map();
-
-    // let stateOptions = ``;
-    // let cityOptions = `<div class='value'> Thane </div>`;
-
-    // stateOptions += `<div class='value'>Andhra Pradesh </div>`;
-
-    /*block.innerHTML = `
-        <div class="dropdowns">
-            <div class="state-dropdown dropdown">
-                <div class="option active">Maharashtra</div>
-                 <div class="state-options options dp-none">
-                     <input type='text' placeholder='city'/>
-                    ${stateOptions}
-                 </div>
-
-            </div>
-
-            <div class="city-dropdown dropdown">
-                <div class="option active">Mumbai</div>
-                 <div class="city-options options dp-none">
-                         <input type='text' placeholder='city'/>
-                        ${cityOptions}
-                 </div>
-            </div>
-        </div>
-        
-       <div class="map-btn-wrapper">
-            <div class="branch-info">
-                <p>Find The Nearest Branch From Your Place</p>
-                <p>Branch - Borivali east</p>
-                <p> Distance - 1.4km </p>
-                <button class="btn-locate">Locate me</button>
-             </div>
-            <div class="map-container">
-            </div>
-        </div>
-        
-        ${branchLocator()}
-    `*/
-
+    
     block.innerHTML = `${branchLocator_dropdown(block)}`;
     block.innerHTML += `${branchLocator_Map(block)}`;
     block.innerHTML += `${branchLocator()}`;
 
-    function myMap(lat, long) {
+    onloadBranchLocator(block);
+
+    /* function myMap(lat, long) {
       var mapProp = {
           center: new google.maps.LatLng(lat, long),
           zoom: 15,
@@ -191,18 +149,7 @@ export default function decorate(block) {
         loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCJr5F6tJXVCcA_VIJreibOtqG9Vf_rb0k").then((resolve) => {
             myMap(lat, lng);
         });
-    });
+    }); */
 
-    // block.querySelectorAll('.dropdown').forEach(function (value, index) {
-    //     value.addEventListener('click', function (e) {
-    //         if (value.querySelector('.state-options')) {
-    //             value.querySelector('.state-options').classList.remove('dp-none');
-    //             value.querySelector('.state-options').classList.add('active');
-    //         } else {
-    //             value.querySelector('.city-options').classList.remove('dp-none');
-    //             value.querySelector('.city-options').classList.add('active');
-    //         }
-    //     });
-    // });
 }
 
