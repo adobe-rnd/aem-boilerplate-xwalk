fe7cb018fe9629dec821b9888d90e88c1f6facb6
Smit Shah <smit.shah@teknopoint.in>
2024-09-02 20:41:59 +0530

    delay added for gtm

--- blocks/branchlocator/branchlocator-api.js
+++ blocks/branchlocator/branchlocator-api.js
@@ -20,3 +20,60 @@ export function initMap(sortedLat, sortedLng) {
   });
   directionsRenderer.setMap(map);
 }
+
+export async function searchBranchByURL(){
+  // let searchBranchURL = location.href;
+  let searchBranchURL = 'http://localhost:3000/branch-locator/delhi';
+  let splitSearch = searchBranchURL.split('/');
+  splitSearch.forEach(function (each){
+      if(each.includes('branch-locator')){
+
+        // Country
+        setLocationObj.geoInfo.country = "India";
+
+        // State Check
+        if((splitSearch.length - 1) >= 4 && splitSearch[4]){
+          setLocationObj.geoInfo.state = splitSearch[4];
+        }else{
+          return false;
+        }
+        
+        // City Check
+        if((splitSearch.length - 1) == 5 && splitSearch[5]){
+          setLocationObj.geoInfo.city = splitSearch[5];
+        }else{
+          
+        }
+
+        // Location Code Check
+        if((splitSearch.length - 1) == 6 && splitSearch[6]){
+          setLocationObj.geoInfo.locationcode = splitSearch[6].split('-').pop();
+        }
+
+        let searchBranchLatLng = "";
+        if(setLocationObj.geoInfo.locationcode){
+          searchBranchLatLng = getExcelData[setLocationObj.geoInfo.state]?.find(eachLocationCode =>
+            eachLocationCode["Location Code"] === setLocationObj.geoInfo.locationcode
+          );
+        }
+
+        if(searchBranchLatLng){
+          setLocationObj.lat = defaultLatLng['Latitude'];
+          setLocationObj.lng = defaultLatLng['Longitude'];
+        }else{
+          if(setLocationObj.geoInfo.state && setLocationObj.geoInfo.city){
+            let stateCityLatLng = getExcelData[setLocationObj.geoInfo.state]?.find(eachCityCheck =>
+              eachCityCheck.City === setLocationObj.geoInfo.city
+            );
+            setLocationObj.lat = stateCityLatLng['Latitude'];
+            setLocationObj.lng = stateCityLatLng['Longitude'];
+          }else if(setLocationObj.geoInfo.state){
+            let stateLatLng = getExcelData[setLocationObj.geoInfo.state][0];
+            setLocationObj.lat = stateLatLng['Latitude'];
+            setLocationObj.lng = stateLatLng['Longitude'];
+          }
+        }
+        return true;
+      }
+  });
+}
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,34 @@ import { sampleRUM } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+function loadHeadGTM() {
+  const scriptTag = document.createElement("script");
+  
+  scriptTag.innerHTML = `<!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T5MTVQ9');</script>
+        <!-- End Google Tag Manager -->`;
+
+  document.head.prepend(scriptTag);
+}
+
+function loadBodyGTM() {
+  const scriptTag = document.createElement("script");
+  
+  scriptTag.innerHTML = `<!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5MTVQ9"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->`;
+        
+  document.body.prepend(scriptTag);
+}
+
+
+
+if(!window.location.hostname.includes('localhost') && !window.location.hostname.includes('author')) {
+    loadHeadGTM();
+    loadBodyGTM();
+} 
