2685905ac477dc9bcd20fa7563e50474d25ce944
François Fornaciari <francois.fornaciari@gmail.com>
2024-09-16 16:17:19 +0200

    Update

--- blocks/banners/banners.js
+++ blocks/banners/banners.js
@@ -1,15 +1,30 @@
 
 export default async function decorate(block) {
-    const reponse = await fetch("https://main--xwalk-automation--fornacif.hlx.page/banners.json");
-    const banners = await reponse.json();
+    const bannersResponse = await fetch("https://main--xwalk-automation--fornacif.hlx.page/banners.json");
+    const bannerIndex = await bannersResponse.json();
+    const bannerPages = bannerIndex.data;
 
-    const content = document.createRange().createContextualFragment(`
-        <div>
-            Banners
-        </div>
-    `);
+    const bannerContents = [];
 
-    block.textContent = '';
-    block.append(content);
+    for (const bannerPage of bannerPages) {
+        const bannerPath = bannerPage.path;
+
+        const bannerResponse = await fetch(bannerPath);
+        const bannerContent = await bannerResponse.text();
+
+        bannerContents.push(bannerContent);
+    }
+
+    let htmlContent = '<div>';
+
+    for (const bannerContent of bannerContents) {
+        htmlContent += bannerContent;
+    }
+    
+    htmlContent += '</div>';
+
+    const content = document.createRange().createContextualFragment(htmlContent);
+
+    block.textContent = htmlContent;
 }
 
