ab258ed248a7079a479409fdd875de960c210143
François Fornaciari <francois.fornaciari@gmail.com>
2024-09-16 12:10:11 +0200

    Update

--- blocks/banners/banners.js
+++ blocks/banners/banners.js
@@ -1,6 +1,8 @@
 
-
 export default async function decorate(block) {
+    const reponse = await fetch("http://example.com/films.json");
+    const films = await reponse.json();
+
     const content = document.createRange().createContextualFragment(`
         <div>
             Banners
