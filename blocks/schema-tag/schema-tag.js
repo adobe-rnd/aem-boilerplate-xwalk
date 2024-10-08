3eec8194d044d798ed65380f36cf665bad48bae6
Om Gupta <om.gupta@teknopoint.in>
2024-10-08 18:15:08 +0530

    Schema Block added

--- blocks/schema-tag/schema-tag.js
+++ blocks/schema-tag/schema-tag.js
@@ -1,3 +1,30 @@
 export default function decorate(block) {
-  document.head.append(block);
+  // document.head += (block.textContent);
+
+  // const schema = {
+  //   "@context": "https://schema.org",
+  //   "@type": "Organization",
+  //   "name": "Apply for a Home loan online with Piramal Finance 1 ",
+  //   "url": "https://www.piramalfinance.com/",
+  //   "logo": "/",
+  //   "contactPoint": {
+  //     "@type": "ContactPoint",
+  //     "telephone": "1800-266-9777",
+  //     "contactType": "customer service",
+  //     "areaServed": "IN",
+  //     "availableLanguage": "en"
+  //   },
+  //   "sameAs": [
+  //     "https://www.facebook.com/pf",
+  //     "https://twitter.com/pf",
+  //     "https://www.instagram.com/pf/",
+  //     "https://www.youtube.com/pfInsurance",
+  //     "https://www.linkedin.com/company/pf"
+  //   ]
+  // }
+  const script = document.createElement('script')
+  script.type = "application/ld+json";
+  script.innerHTML = block.textContent;
+  block.innerHTML = '';
+  document.head.append(script)
 }
