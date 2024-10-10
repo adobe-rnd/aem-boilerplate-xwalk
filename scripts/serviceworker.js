4d858d44a01c2a1ad249edb40e908837e9821776
Smit Shah <smit.shah@teknopoint.in>
2024-10-10 16:52:39 +0530

    0.3 dc adding

--- scripts/serviceworker.js
+++ scripts/serviceworker.js
@@ -1,3 +1,3 @@
 // Import external script from MoEngage CDN
 
-importScripts("https://cdn.moengage.com/release/dc_3/serviceworker_cdn.min.latest.js");
+importScripts("https://cdn.moengage.com/release/dc_0.3/serviceworker_cdn.min.latest.js");
