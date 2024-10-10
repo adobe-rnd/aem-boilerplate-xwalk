8f2611de080ae4c06da9776dd43424caca934eb5
Smit Shah <smit.shah@teknopoint.in>
2024-10-10 13:29:33 +0530

    delayed js service worker added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -898,34 +898,6 @@ function onCLickApplyFormOpen(e) {
   e.preventDefault();
 }
 
-function renderSchema() {
-  const schema = {
-    '@context': 'https://schema.org',
-    '@type': 'Organization',
-    name: 'Apply for a Home loan online with Piramal Finance 1 ',
-    url: 'https://www.piramalfinance.com/',
-    logo: '/',
-    contactPoint: {
-      '@type': 'ContactPoint',
-      telephone: '1800-266-9777',
-      contactType: 'customer service',
-      areaServed: 'IN',
-      availableLanguage: 'en',
-    },
-    sameAs: [
-      'https://www.facebook.com/pf',
-      'https://twitter.com/pf',
-      'https://www.instagram.com/pf/',
-      'https://www.youtube.com/pfInsurance',
-      'https://www.linkedin.com/company/pf',
-    ],
-  };
-  const script = document.createElement('script');
-  script.type = 'application/ld+json';
-  script.innerHTML = JSON.stringify(schema);
-  document.head.append(script);
-}
-renderSchema();
 
 export function getDay() {
   const date = new Date();
---
+++ scripts/serviceworker.js
@@ -0,0 +1,3 @@
+// Import external script from MoEngage CDN
+
+importScripts("https://cdn.moengage.com/release/Dc_3/serviceworker_cdn.min.latest.js");
