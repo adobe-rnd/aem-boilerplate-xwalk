c9f3430c41283c9a9d701fb5e9ce05887be232b5
Vaibhav sasulkar <vaibhav.sasulkar@teknopoint.local>
2024-10-10 16:42:35 +0530

    table authoring change

--- blocks/code/code.js
+++ blocks/code/code.js
@@ -1,21 +1,50 @@
+import { decorateIcons } from "../../scripts/aem.js";
 import { decoratePlaceholder } from "../../scripts/scripts.js";
 import { mobileHeaderAnalytics } from "./code-analytics.js";
 import { decorateTable } from "./table.js";
-
 export default async function decorate(block) {
-    block.innerHTML = await decoratePlaceholder(block);
-    if(block.classList.contains('table')){
-        decorateTable(block)
-        return block
-    }
-    const titleData = block.children[0]?.querySelector('p')?.textContent.trim() || '';
-    block.innerHTML = '';
-    if (titleData) {
-        const titleElement = document.createElement('div');
-        titleElement.innerHTML = titleData;
-        block.append(titleElement);
-    }
+  console.log(block.classList.toString());
+  block.innerHTML = await decoratePlaceholder(block);
+  if (block.classList.contains('table')) {
+    decorateTable(block)
+    return block
+  }
+  const titleData = block.children[0]?.querySelector('p')?.innerHTML.trim().replaceAll('&lt;', '<').replaceAll('&gt;', '>') || '';
+  block.innerHTML = '';
+  if (titleData) {
+    const titleElement = document.createElement('div');
+    titleElement.innerHTML = titleData;
+    const theads = titleElement.querySelectorAll('tr');
+    theads.forEach(function (thead, index) {
+      Array.from(thead.children).forEach(function (eachTd) {
+        if (!index) {
+          eachTd.remove();
+          const th = document.createElement('th');
+          th.innerHTML = (eachTd.innerHTML);
+          thead.append(th)
+        }
 
+        if (eachTd.innerHTML.includes('img') && eachTd.textContent.trim()) {
+          const p = document.createElement('p');
+          const span = document.createElement('span');
+          span.append(eachTd.textContent.trim());
+          p.append(eachTd.querySelector('img'))
+          p.append(span)
+          eachTd.innerHTML = '';
+          eachTd.append(p);
+        } else if (eachTd.innerHTML.includes('img')) {
+          // eachTd.remove();
+          // debugger
+          const img = eachTd.querySelector('img');
+          eachTd.innerHTML = '';
+          eachTd.append(img);
+        } else {
+          eachTd.innerHTML = eachTd.innerHTML;
+        }
+      })
+    })
+    block.append(titleElement);
+  }
   const iframe = block.querySelectorAll('iframe');
   if (iframe.length != 0) {
     block.parentElement.classList.add('iframe-width');
