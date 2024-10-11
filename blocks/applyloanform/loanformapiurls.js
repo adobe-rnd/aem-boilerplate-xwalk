191817f1dd117c6701ac1b2408a02532a8e910ba
Smit Shah <smit.shah@teknopoint.in>
2024-10-11 17:02:57 +0530

    url change added

--- blocks/applyloanform/loanformapiurls.js
+++ blocks/applyloanform/loanformapiurls.js
@@ -23,9 +23,14 @@ const resendOTPUrl = 'https://campaignapi.piramalfinance.com/api/website/resend-
 const verifyOTPURL = 'https://campaignapi.piramalfinance.com/api/website/verify-otp';
 
 // V1 API URL
+// const leadAPIURL = 'https://mobileapi.pchf.in/api/website/insert-lead-data';
+
+// V2 API URL
+const leadAPIURL = 'https://campaignapi.piramalfinance.com/api/website/insert-lead-data';
+
+// V1 API URL
 const otpTokenURL = 'https://apisit.piramal.com/oauth/v1/cc/accesstoken';
 const smsURL = 'https://apisit.piramal.com/communication/v2/karix/sms-query?ver=1.0&key=HbUcyrewWm27hdKRZyj2zA%3D%3D&encrypt=0&send=PIRCHF&dest=8007359739&text=Please%20use%20this%20one-time%20password%20for%20Piramal%20User%20Account%20Verification%3A%209106.%20DO%20NOT%20SHARE%20THIS%20WITH%20ANYONE%20and%20it%20is%20valid%20for%205%20minutes%20only';
-const leadAPIURL = 'https://mobileapi.pchf.in/api/website/insert-lead-data';
 const getWhatAPIAuthURL = 'https://piramal.allincall.in/campaign/external/get-auth-token/';
 const getWhatAPIURL = 'https://piramal.allincall.in/campaign/external/send-event-based-triggered-whatsapp-campaign/';
 
