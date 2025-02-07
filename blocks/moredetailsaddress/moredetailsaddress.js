45e461b03996bdbfda3ca5ff8e0b6a9ba8416802
npy11 <Neeraj.yadav@teknopoint.local>
2025-02-07 12:30:48 +0530

    branch locator breadcrumb resolved

--- blocks/moredetailsaddress/moredetailsaddress.js
+++ blocks/moredetailsaddress/moredetailsaddress.js
@@ -85,15 +85,16 @@ async function onbranchDetails() {
   const userLocation = await returnLatLan();
   if (userLocation) {
     setLocationObj.distance = calculateDistance(setLocationObj.lat, setLocationObj.lng, userLocation.lat, userLocation.lng);
-    const distanceElement = document.querySelector(".address-info ul li:nth-child(3)");
+    // const distanceElement = document.querySelector(".address-info ul li:nth-child(3)");
+    let distanceElement = document.createElement("li");
+    const addressUL = document.querySelector(".address-info ul");
     if (setLocationObj.distance <= 40) {
       distanceElement.innerText = `${setLocationObj.distance.toFixed()} Km away from your location`;
+      addressUL.append(distanceElement)
     } else {
       distanceElement.remove();
     }
-  } else {
-    document.querySelector(".address-info ul li:nth-child(3)").remove();
-  }
+  } 
 
   // setLocationObj.storedata = sortingNearestBranch(setLocationObj.lat, setLocationObj.lng, setLocationObj.getExcelData);
   setLocationObj.storedata = sortByCityandState(setLocationObj.getExcelData[setLocationObj.geoInfo.state]);
