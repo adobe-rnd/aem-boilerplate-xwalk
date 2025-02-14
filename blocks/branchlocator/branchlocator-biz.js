5de3eaf95b2cd30a29f5788fcc35a4ab03f27824
Smit Shah <smit.shah@teknopoint.in>
2025-02-14 12:15:51 +0530

    class change

--- blocks/branchlocator/branchlocator-biz.js
+++ blocks/branchlocator/branchlocator-biz.js
@@ -387,9 +387,9 @@ async function handleLocateMeClick(e, block) {
 
 function updateUIForNoLocation(block, target) {
   block.closest(".section").querySelector(".nearest-txt").textContent = "Kindly enable your Location and Refresh the page";
-  if(target.closest(".btn-locate-details").getAttribute('hideButton') != 'true'){
+  if(target.closest(".btn-locate").getAttribute('hideButton') != 'true'){
     target.classList.add("dp-none");
-    target.closest(".btn-locate-details").setAttribute('hideButton', true);
+    target.closest(".btn-locate").setAttribute('hideButton', true);
   }
 }
 
